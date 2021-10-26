b911251bc8742a37716d6255d53a5cf3664f7634
Ignas Augustinavicius <ignas.augustinavicius@gmail.com>
2021-10-26 08:39:58 -0700

    Bug Fix

--- database/connect.js
+++ database/connect.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Connection
-module.exports = mongoose.connect(process.env.MONGO_SRV);
+module.exports = mongoose.connect(process.env.MONGO_SRV, { useNewUrlParser: true });
