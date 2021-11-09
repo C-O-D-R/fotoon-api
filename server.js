f5c6e56d26fb233421967474bbfdb355d196b7df
Ignas Augustinavicius <ignas.augustinavicius@gmail.com>
2021-11-09 08:32:55 -0800

    SSL

--- server.js
+++ server.js
@@ -16,6 +16,22 @@ mongoose.connect(process.env.MONGO_SRV, { useNewUrlParser: true, useUnifiedTopol
 // Server
 const server = express();
 
+// SSL
+require("greenlock-express")
+    .init({
+        packageRoot: __dirname,
+        configDir: "./greenlock.d",
+ 
+        // contact for security and critical bug notices
+        maintainerEmail: "belenkas@belenka.com",
+ 
+        // whether or not to run at cloudscale
+        cluster: false
+    })
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(server);
+
 // Parser
 server.use(bodyParser.json());
 
