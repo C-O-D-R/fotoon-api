0e6db785283e21811dabbf3629a7f3506bab404d
alojine <76651648+alojine@users.noreply.github.com>
2021-12-06 15:14:13 +0200

    Get user data

--- api/user.js
+++ api/user.js
@@ -46,13 +46,41 @@ Router.patch('/update', authUser, async (req, res) => {
     }
 });
 
-Router.get('/test', authUser, async (req, res) => {
-    // Global Variables
-    const userId = req.user.id;
+Router.get('/test/:id', async (req, res) => {
+    // 1. Save ID
+    const userId = req.params.id;
 
-    // Request Database User
+    // 2. Find User By ID in DB
     const dbUser = await UserSchema.findOne({ _id: userId }).lean();
+
+    /// svarbu
     const buffer = await Buffer.from(dbUser.profilePicture, 'base64');
     console.log(buffer);
     fs.writeFileSync('image.jpg', buffer);
+});
+
+// Get user
+Router.get('/:id', async (req, res) => {
+    // Global variables
+    const userId = req.params.id;
+
+    // Checking for DB and User
+    try {
+        // Wrong ID Format
+        if (!userId.match(/^[0-9a-fA-F]{24}$/)) return res.status(400).json({ status: 'error', code: 'invalid_format', description: `Invalid format` });
+
+        // Request Database User
+        const dbUser = await UserSchema.findOne({ _id: userId }).lean();
+
+        // User Not Found
+        if (!dbUser) return res.status(404).json({ status: 'error', code: 'invalid_user', description: `User not found` });
+
+        // Success User Data
+        return res.status(200).json({ username: dbUser.username, profilePicture: dbUser.profilePicture });
+
+    } catch (error) {
+        // Failed User Data
+        terminal.error(`[SERVER] Failed at user: ${error}`);
+        return res.status(500).json({ status: 'error', code: 'server_error', description: `Internal server error ${error}` });
+    }
 });
