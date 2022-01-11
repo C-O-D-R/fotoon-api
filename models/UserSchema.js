3270a399e02ba79d879c190c38bf3ac39760b187
augustinavicius <augustinavicius.ignas@gmail.com>
2022-01-11 19:26:08 +0200

    New User Model

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -5,7 +5,9 @@ import mongoose from 'mongoose';
 export default mongoose.model('UserSchema', new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    profilePicture: { type: String, required: false },
-    bio: { type: String, required: false},
-    following: { type: Array, required: false}
+    avatar: { type: String, required: false },
+    shortBio: { type: String, required: false },
+    longBio: { type: String, required: false },
+    following: { type: Array, required: false },
+    followers: { type: Array, required: false }
 }, { versionKey: false, collection: 'users' }));
