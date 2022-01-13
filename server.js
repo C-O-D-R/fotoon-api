650389a35983d4749938ae22377e208f34f29820
augustinavicius <augustinavicius.ignas@gmail.com>
2022-01-13 23:22:49 +0200

    Default Avatar Image

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const swaggerDocsOptions = {
         openapi: '3.0.0',
         info: {
             title: 'Fotoon API',
-            version: '1.0.1',
+            version: '1.0.2',
             description: 'Fotoon API sukurtas KA projektui.',
 
             servers: [{
